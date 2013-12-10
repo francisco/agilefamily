92da529563bc6fcc7a226bc030cb3076f6a6a601
Francisco ALberini <f.alberini@gmail.com>
2013-12-10 11:28:20 -0800

    First commit with rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgilefamilyAlpha::Application.config.secret_token = 'b5fea4925f213c734c45dec05e56c300fd6263149d848b8bdd2cf29f0f55332e6a6df8fb22f30afa0d42ea9400bbe2c5fc521e9f8d9926c1fd1fc6d34faca205'
