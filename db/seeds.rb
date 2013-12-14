4ec55063741d51c74429b951517f75e465b7b67b
Francisco ALberini <f.alberini@gmail.com>
2013-12-14 13:51:48 -0800

    Weekday table seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Weekday.create!(weekday: "monday")
+Weekday.create!(weekday: "tuesday")
+Weekday.create!(weekday: "wednesday")
+Weekday.create!(weekday: "thursday")
+Weekday.create!(weekday: "friday")
+Weekday.create!(weekday: "saturday")
+Weekday.create!(weekday: "sunday")
